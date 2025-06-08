416b4235c79f9bdec6184832c1daf886190f81c6
Zhefan Xu <zhefanx@andrew.cmu.edu>
2025-06-07 23:23:53 -0400

    Update setup_deployment.sh

--- isaac-training/setup_deployment.sh
+++ isaac-training/setup_deployment.sh
@@ -12,7 +12,7 @@ eval "$(conda shell.bash hook)"
 
 # Step 1: Create conda env with python3.10
 echo "Setting up conda env..."
-conda create -n $ENV_NAME python=3.10
+conda create -n $ENV_NAME python=3.10 -c conda-forge
 conda activate $ENV_NAME
 pip install numpy==1.26.4
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
